20ae8c9e7905aa5c242d837d8bd91046d717013c
Aleksandr Pryakhin <aleksandr@contabilizei.com.br>
2018-10-01 17:21:19 -0300

    Find category by entrance of specified letter.

--- src/main/java/br/com/bb/controller/CategoryController.java
+++ src/main/java/br/com/bb/controller/CategoryController.java
@@ -4,6 +4,7 @@ import br.com.bb.model.Category;
 import br.com.bb.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,10 @@ public class CategoryController {
     @GetMapping("/listAll")
     public List<Category> listAll(){
         return categoryService.listAll();
+    }
+
+    @GetMapping("/getByLetter/{letter}")
+    public Category getByLetter(@PathVariable String letter){
+        return categoryService.getByLetter(letter);
     }
 }
--- src/main/java/br/com/bb/service/CategoryService.java
+++ src/main/java/br/com/bb/service/CategoryService.java
@@ -4,8 +4,12 @@ import br.com.bb.model.Category;
 import br.com.bb.repository.CategoryDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import java.text.Normalizer;
+import java.util.Comparator;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Service
 public class CategoryService {
@@ -20,4 +24,37 @@ public class CategoryService {
     public List<Category> listAll() {
         return dao.findAll();
     }
+
+    public Category getByLetter(String letter) {
+        return dao.findAll().stream().max(Comparator.comparing(category -> count(category.getName(), letter))).orElse(null);
+    }
+
+    private int count(String srcStr, String letter) {
+        return StringUtils.countOccurrencesOf(stripAccents(srcStr).toLowerCase(), letter);
+    }
+
+    //copiado sem vergonha de Apache StringUtils
+    private String stripAccents(final String input) {
+        if (input == null) {
+            return null;
+        }
+        final Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");//$NON-NLS-1$
+        final StringBuilder decomposed = new StringBuilder(Normalizer.normalize(input, Normalizer.Form.NFD));
+        convertRemainingAccentCharacters(decomposed);
+        // Note that this doesn't correctly remove ligatures...
+        return pattern.matcher(decomposed).replaceAll("");
+    }
+
+    private void convertRemainingAccentCharacters(final StringBuilder decomposed) {
+        for (int i = 0; i < decomposed.length(); i++) {
+            if (decomposed.charAt(i) == '\u0141') {
+                decomposed.deleteCharAt(i);
+                decomposed.insert(i, 'L');
+            } else if (decomposed.charAt(i) == '\u0142') {
+                decomposed.deleteCharAt(i);
+                decomposed.insert(i, 'l');
+            }
+        }
+    }
+
 }
